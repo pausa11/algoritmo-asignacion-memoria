9588f9d98c10b5f8e9b5ef236e269e7a33f2a145
Daniel Toro soto <daniel.toro1@utp.edu.co>
2024-10-15 03:11:20 -0500

    estilos mejorados

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
+import './App.css';
 
 function App() {
 
   const [rowsNumber, setRowsNumber] = useState(3);
   const [referenceString, setReferenceString] = useState('');
+  const [showResults, setShowResults] = useState(false);
 
   const referenceStringToArray = () => {
     return referenceString.split(' ');
@@ -23,7 +25,7 @@ function App() {
 
     for (let i = 0; i < referenceArray.length; i++) {
       let currentPage = referenceArray[i];
-
+      let mark = false;
       // Si la página no está en los marcos
       if (!fifoFrames.includes(currentPage)) {
         if (queue.length < frames) {
@@ -37,8 +39,14 @@ function App() {
         }
         queue.push(currentPage);
         faultsCounter++;
+        mark = true;
       }
+      //marca con un f, a la posición que se reemplazó la página 
       fifoChartArray.push([...fifoFrames]);
+      if(mark){
+        const index = fifoChartArray.length - 1;
+        fifoChartArray[index][fifoFrames.indexOf(currentPage)] = `${currentPage}f`;
+      }
     }
     setFifosFault(faultsCounter);
     setFifoChart(rotateMatrix(fifoChartArray));
@@ -58,7 +66,7 @@ function App() {
 
     for (let i = 0; i < referenceArray.length; i++) {
       let currentPage = referenceArray[i];
-
+      let mark = false;
       // Si la página no está en los marcos
       if (!lruFrames.includes(currentPage)) {
         if (queue.length < frames) {
@@ -72,6 +80,7 @@ function App() {
         }
         queue.push(currentPage);
         faultsCounter++;
+        mark = true;
       } else {
         // Actualiza la página en la cola
         const index = queue.indexOf(currentPage);
@@ -79,6 +88,11 @@ function App() {
         queue.push(currentPage);
       }
       lruChartArray.push([...lruFrames]);
+      if(mark){
+        const index = lruChartArray.length - 1;
+        lruChartArray[index][lruFrames.indexOf(currentPage)] = `${currentPage}f`;
+      }
+
     }
     setLrusFault(faultsCounter);
     setLruChart(rotateMatrix(lruChartArray));
@@ -89,7 +103,7 @@ function App() {
   const [optimalChart, setOptimalChart] = useState([]);
   const [optimalsFault,setOptimalsFault] = useState(0);
   const optimal = () => {
-    const referenceArray = [6, 1, 7, 1, 2, 1, 5, 6, 0, 1, 7, 1, 1, 6, 0, 7, 0, 1, 2, 6, 1]; 
+    const referenceArray = referenceStringToArray();
     const rowsNumberToInteger = parseInt(rowsNumber);
     const frames = rowsNumberToInteger;
     let optimalFrames = Array(frames).fill(null); 
@@ -98,6 +112,7 @@ function App() {
   
     for (let i = 0; i < referenceArray.length; i++) {
       let currentPage = referenceArray[i];
+      let mark = false;
   
       if (!optimalFrames.includes(currentPage)) {
         if (optimalFrames.includes(null)) {
@@ -110,8 +125,6 @@ function App() {
           for (let j = 0; j < optimalFrames.length; j++) {
             const page = optimalFrames[j];
             const distance = referenceArray.slice(i + 1).indexOf(page);
-  
-            console.log(`Page: ${page}, Distance: ${distance}`);
   
             if (distance === -1) {
               replaceIndex = j;
@@ -127,9 +140,14 @@ function App() {
         }
   
         faultsCounter++;
+        mark = true;
       }
   
       optimalChartArray.push([...optimalFrames]);
+      if(mark){
+        const index = optimalChartArray.length - 1;
+        optimalChartArray[index][optimalFrames.indexOf(currentPage)] = `${currentPage}f`;
+      }
     }
   
     setOptimalsFault(faultsCounter);
@@ -152,6 +170,7 @@ function App() {
 
     for (let i = 0; i < referenceArray.length; i++) {
       let currentPage = referenceArray[i];
+      let mark = false;
 
       // Si la página no está en los marcos
       if (!secondChanceFrames.includes(currentPage)) {
@@ -178,6 +197,7 @@ function App() {
           }
         }
         faultsCounter++;
+        mark = true;
       }
       else {
         // Actualiza la página en la cola
@@ -188,6 +208,10 @@ function App() {
         }
       }
       secondChanceChartArray.push([...secondChanceFrames]);
+      if(mark){
+        const index = secondChanceChartArray.length - 1;
+        secondChanceChartArray[index][secondChanceFrames.indexOf(currentPage)] = `${currentPage}f`;
+      }
     }
     setUpgradedsFault(faultsCounter);
     setUpgradedChart(rotateMatrix(secondChanceChartArray));
@@ -212,142 +236,202 @@ function App() {
 
   //--------------------------------------------------------------------------------
 
+  const isReady = () => {
+    if (referenceString !== '' && rowsNumber !== '') {
+      setShowResults(true);
+    }
+    else {
+      alert('Por favor, llena todos los campos');
+    }
+  }
+
+  //--------------------------------------------------------------------------------
+
   return (
-    <div style={{height:'100vh',width:'100vw', background:'white',display:'flex',flexDirection:'column',alignItems:'center'}}>
+    <div style={{height:'100vh',width:'100vw', background:'white',display:'flex',flexDirection:'column',alignItems:'center',textAlign:'center'}}>
       
-      <h1>Algoritmo de remplazo de páginas</h1>
-
+      <h1 style={{textShadow: '0 0 1vh #8080803d',margin:'2vh'}}>Algoritmos de asignación de memoria</h1>
       <div id='inputsSelector' style={{display:'flex',flexDirection:'column',alignItems:'center',width:'100%'}}>
-        <label>Ingresa la cadena de referencia</label>
-        <input type='text' value={referenceString} onChange={(e)=>setReferenceString(e.target.value)} placeholder='Ejemplo: 1 2 3 4 1 2 5 1 2 3 4 5' />
-        <label>Selecciona el número de filas</label>
-        <input type='number' value={rowsNumber} onChange={(e)=>setRowsNumber(e.target.value)} min={2} max={10} />
-        <button onClick={()=>{fifo();lru();optimal();secondChance()}}>Calcular</button>
+        <label style={{margin:'1vh'}}>Ingresa la cadena de referencia</label>
+        <input type='text' value={referenceString} onChange={(e)=>setReferenceString(e.target.value)} placeholder='Ejemplo: 1 2 3 4 1 2 5 1 2 3 4 5' style={{width:'30%',borderRadius:'1vh',padding:'1ch',border:'.2vh solid #8080803d',display:'flex',justifyContent:'center',alignItems:'center',textAlign:'center',boxShadow:'0 0 1vh #8080803d'}} />
+        <label style={{margin:'1vh'}}>Selecciona el número de filas</label>
+        <input type='number' value={rowsNumber} onChange={(e)=>setRowsNumber(e.target.value)} min={2} max={10} style={{width:'30%',borderRadius:'1vh',padding:'1ch',border:'.2vh solid #8080803d',display:'flex',justifyContent:'center',alignItems:'center',textAlign:'center',boxShadow:'0 0 1vh #8080803d'}} />
+        <button onClick={()=>{fifo();lru();optimal();secondChance();isReady()}} style={{margin:'1vh',borderRadius:'1vh',padding:'1ch',border:'none',display:'flex',justifyContent:'center',alignItems:'center',textAlign:'center',background:'#005eff',color:'white',fontWeight:'500',boxShadow:'0 0 1vh #005eff',fontSize:'2ch',marginTop:'1ch',letterSpacing:'.15vw'}}>Calcular</button>
       </div>
 
-      <div id='fifoChart' style={{display:'flex',flexDirection:'column',alignItems:'center',width:'100%'}}>
-        <h2>FIFO</h2>
-        <table>
-          <thead>
-            <tr >
+      {
+        showResults && (
+        <div className='animation' style={{display:'flex',flexDirection:'column',alignItems:'center',width:'100%'}}>
+        <div id='fifoChart' style={{display:'flex',flexDirection:'column',alignItems:'center',width:'100%'}}>
+          <h2>FIFO</h2>
+          <table style={{borderCollapse: 'collapse', margin: 'auto'}}>
+            <thead>
+              <tr>
+                {
+                  referenceStringToArray().map((item, index) => {
+                    return (
+                      <th key={index} style={{width: '2vw'}}>
+                        {item}
+                      </th>
+                    );
+                  })
+                }
+              </tr>
+            </thead>
+            <tbody>
               {
-                referenceStringToArray().map((item,index)=>{
-                  return <th key={index} style={{width:'2vw'}}>{item}</th>
-                }) 
+                fifoChart.map((frame, index) => {
+                  return (
+                    <tr key={index}>
+                      {
+                        frame.map((item, index) => {
+                          return (
+                            <td 
+                              key={index} 
+                              style={{
+                                width: '2vw', 
+                                border: '1px solid black', 
+                                textAlign: 'center', 
+                                color: 'black',
+                                padding: '5px',
+                                background: typeof item === 'string' && item.includes('f') ? 'rgba(255,0,0,0.6)' : 'white'
+                              }}>
+                              {typeof item === 'string' ? item.replace('f','') : item}
+                            </td>
+                          );
+                        })
+                      }
+                    </tr>
+                  );
+                })
               }
-            </tr>
-          </thead>
-          <tbody>
-            {
-              fifoChart.map((frame,index)=>{
-                return (
-                  <tr key={index}>
-                    {
-                      frame.map((item,index)=>{
-                        return <td key={index} style={{width:'2vw'}}>{item}</td>
-                      })
-                    }
-                  </tr>
-                )
-              })
-            }
-          </tbody>
-        </table>
-        <p>Fallos: {fifosFault}</p>
-      </div> 
+            </tbody>
+          </table>
+          <p>Fallos: {fifosFault}</p>
+        </div>
 
-      <div id='lruChart' style={{display:'flex',flexDirection:'column',alignItems:'center',width:'100%'}}>
-        <h2>LRU</h2>
-        <table>
-          <thead>
-            <tr >
+        <div id='lruChart' style={{display:'flex',flexDirection:'column',alignItems:'center',width:'100%'}}>
+          <h2>LRU</h2>
+          <table style={{borderCollapse: 'collapse', margin: 'auto'}}>
+            <thead>
+              <tr >
+                {
+                  referenceStringToArray().map((item,index)=>{
+                    return <th key={index} style={{width:'2vw'}}>{item}</th>
+                  }) 
+                }
+              </tr>
+            </thead>
+            <tbody>
               {
-                referenceStringToArray().map((item,index)=>{
-                  return <th key={index} style={{width:'2vw'}}>{item}</th>
-                }) 
+                lruChart.map((frame,index)=>{
+                  return (
+                    <tr key={index}>
+                      {
+                        frame.map((item,index)=>{
+                          return                           <td 
+                          key={index} 
+                          style={{
+                            width: '2vw', 
+                            border: '1px solid black', 
+                            textAlign: 'center', 
+                            color: 'black',
+                            padding: '5px',
+                            background: typeof item === 'string' && item.includes('f') ? '#0040ff99' : 'white'
+                          }}>{typeof item === 'string' ? item.replace('f','') : item}</td>
+                        })
+                      }
+                    </tr>
+                  )
+                })
               }
-            </tr>
-          </thead>
-          <tbody>
-            {
-              lruChart.map((frame,index)=>{
-                return (
-                  <tr key={index}>
-                    {
-                      frame.map((item,index)=>{
-                        return <td key={index} style={{width:'2vw'}}>{item}</td>
-                      })
-                    }
-                  </tr>
-                )
-              })
-            }
-          </tbody>
-        </table>
-        <p>Fallos: {lrusFault}</p>
-      </div> 
+            </tbody>
+          </table>
+          <p>Fallos: {lrusFault}</p>
+        </div> 
 
-      <div id='optimalChart' style={{display:'flex',flexDirection:'column',alignItems:'center',width:'100%'}}>
-        <h2>Óptimo</h2>
-        <table>
-          <thead>
-            <tr >
+        <div id='optimalChart' style={{display:'flex',flexDirection:'column',alignItems:'center',width:'100%'}}>
+          <h2>Óptimo</h2>
+          <table style={{borderCollapse: 'collapse', margin: 'auto'}}>
+            <thead>
+              <tr >
+                {
+                  referenceStringToArray().map((item,index)=>{
+                    return <th key={index} style={{width:'2vw'}}>{item}</th>
+                  }) 
+                }
+              </tr>
+            </thead>
+            <tbody>
               {
-                referenceStringToArray().map((item,index)=>{
-                  return <th key={index} style={{width:'2vw'}}>{item}</th>
-                }) 
+                optimalChart.map((frame,index)=>{
+                  return (
+                    <tr key={index}>
+                      {
+                        frame.map((item,index)=>{
+                          return <td 
+                          key={index} 
+                          style={{
+                            width: '2vw', 
+                            border: '1px solid black', 
+                            textAlign: 'center', 
+                            color: 'black',
+                            padding: '5px',
+                            background: typeof item === 'string' && item.includes('f') ? '#2eff0099' : 'white'
+                          }}>{typeof item === 'string' ? item.replace('f','') : item}</td>
+                        })
+                      }
+                    </tr>
+                  )
+                })
               }
-            </tr>
-          </thead>
-          <tbody>
-            {
-              optimalChart.map((frame,index)=>{
-                return (
-                  <tr key={index}>
-                    {
-                      frame.map((item,index)=>{
-                        return <td key={index} style={{width:'2vw'}}>{item}</td>
-                      })
-                    }
-                  </tr>
-                )
-              })
-            }
-          </tbody>
-        </table>
-        <p>Fallos: {optimalsFault}</p>
-      </div>
+            </tbody>
+          </table>
+          <p>Fallos: {optimalsFault}</p>
+        </div>
 
-      <div id='upgradedChart' style={{display:'flex',flexDirection:'column',alignItems:'center',width:'100%'}}>
-        <h2>Mejorado</h2>
-        <table>
-          <thead>
-            <tr >
+        <div id='upgradedChart' style={{display:'flex',flexDirection:'column',alignItems:'center',width:'100%'}}>
+          <h2>Mejorado</h2>
+          <table style={{borderCollapse: 'collapse', margin: 'auto'}}>
+            <thead>
+              <tr >
+                {
+                  referenceStringToArray().map((item,index)=>{
+                    return <th key={index} style={{width:'2vw'}}>{item}</th>
+                  }) 
+                }
+              </tr>
+            </thead>
+            <tbody>
               {
-                referenceStringToArray().map((item,index)=>{
-                  return <th key={index} style={{width:'2vw'}}>{item}</th>
-                }) 
+                upgradedChart.map((frame,index)=>{
+                  return (
+                    <tr key={index}>
+                      {
+                        frame.map((item,index)=>{
+                          return                           <td 
+                          key={index} 
+                          style={{
+                            width: '2vw', 
+                            border: '1px solid black', 
+                            textAlign: 'center', 
+                            color: 'black',
+                            padding: '5px',
+                            background: typeof item === 'string' && item.includes('f') ? '#e500ff99' : 'white'
+                          }}>{typeof item === 'string' ? item.replace('f','') : item}</td>
+                        })
+                      }
+                    </tr>
+                  )
+                })
               }
-            </tr>
-          </thead>
-          <tbody>
-            {
-              upgradedChart.map((frame,index)=>{
-                return (
-                  <tr key={index}>
-                    {
-                      frame.map((item,index)=>{
-                        return <td key={index} style={{width:'2vw'}}>{item}</td>
-                      })
-                    }
-                  </tr>
-                )
-              })
-            }
-          </tbody>
-        </table>
-        <p>Fallos: {upgradedsFault}</p>
-      </div>
+            </tbody>
+          </table>
+          <p>Fallos: {upgradedsFault}</p>
+        </div>
+        </div>
+        )
+      }
 
     </div>
   );
